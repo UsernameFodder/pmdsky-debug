c28ec291a4ba771a52b2b9192abb61539cb65848
EleventhNov <28666422+EleventhNov@users.noreply.github.com>
2022-02-24 14:37:16 -0500

    Fix more stuff

--- headers/types/common/common.h
+++ headers/types/common/common.h
@@ -49,12 +49,13 @@ ASSERT_SIZE(struct preprocessor_flags, 4);
 struct preprocessor_args {
     uint32_t flag_vals[4]; // These are usually IDs with additional flags attached to them
     uint32_t id_vals[5];
-    int32_t number_vals[5]; 
+    int32_t number_vals[5];
     char* strings[5];
-    uint32_t speaker_id; // An optional argument that is used to insert the name of a Pokémon
+    // An optional argument that is used to insert the name of a Pokémon
     // When they're talking through a message box. It requires it's respective flag to be on
+    uint32_t speaker_id;
 };
-ASSERT_SIZE(preprocessor_args, 80);
+ASSERT_SIZE(struct preprocessor_args, 80);
 
 struct type_matchup_16 {
     enum type_matchup val : 16;
