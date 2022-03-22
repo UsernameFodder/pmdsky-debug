3a2353f27db8cc75630ff3eb4a3eb7dbff422e9a
EleventhNov <28666422+EleventhNov@users.noreply.github.com>
2022-03-21 20:22:14 -0500

    Reduced load, fixed formatting

--- headers/functions/kaomado.h
+++ headers/functions/kaomado.h
@@ -9,10 +9,3 @@ void SetKaomadoOffset(struct kaomado*, struct portrait_offset* offset);
 void AllowKaomadoDefault(struct kaomado*, bool allow);
 void IsValidKaomado(struct kaomado*);
 bool LoadKaomado(struct kaomado*, void* buffer); // This function updates all three of the flip fields!
-
-uint8_t CreateKaomadoCanvas(enum screen, uint32_t palette_idx, bool is_framed);
-void FreeKaomadoCanvas(uint8_t canvas); // Disables the canvas and frees resources
-bool CheckKaomadoCanvasBusy(uint8_t canvas); // Busy means the canvas is updating in any way
-void CommitKaomadoCanvas(uint8_t canvas, struct kaomado*); // Writes the passed Kaomado to the canvas buffer
-void HideKaomadoCanvas(uint8_t canvas);
-void UpdateKaomadoCanvas(uint8_t canvas); // Not to be called manually
