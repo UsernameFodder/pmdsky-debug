910dc59a114f03899374a96215afffdd0f4bd0bc
UsernameFodder <usernamefodder@gmail.com>
2022-11-06 15:41:12 -0600

    Port overlay_0031 from Irdkwia's ASM.ods
    Omit the English string literals, since they're obvious when reverse
    engineering and useless when writing patches:
    - "[dungeon:0]"
    - "  ???"
    - "[M:H0]"
    - "[M:H1]"
    - "[M:H2]"
    - "[M:H3]"
    - "[M:H4]"
    - "[M:H5]"
    - "[M:H6]"
    - "[M:H7]"
    - "[string:0][CLUM_SET:80][string:1]"
    - "[value:0:3] [CS:%c]%s[CR]"
    - "[M:S2][tactics:0]"
    - "[M:S1][tactics:0]"

--- headers/functions/overlay31.h
+++ headers/functions/overlay31.h
@@ -5,5 +5,7 @@ undefined TeamMenu(struct entity* leader);
 void RestMenu(void);
 int RecruitmentSearchMenuLoop(void);
 int HelpMenuLoop(void);
+void EntryOverlay31(void);
+void DungeonMenuSwitch(int idx);
 
 #endif
