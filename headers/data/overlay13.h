282d18a29603a8094bb16c275e6407a1da2314e6
UsernameFodder <usernamefodder@gmail.com>
2022-11-05 22:08:47 -0500

    Port overlay_0013 from Irdkwia's ASM.ods
    Omit the string literals, since they're obvious when reverse engineering
    and useless when writing patches:
    - "[gender:0][CLUM_SET:28][kind:0]"

--- headers/data/overlay13.h
+++ headers/data/overlay13.h
@@ -6,6 +6,8 @@ extern struct monster_id_16 STARTERS_HERO_IDS[32];
 extern uint16_t STARTERS_STRINGS[48];
 extern uint16_t QUIZ_QUESTION_STRINGS[66];
 extern uint16_t QUIZ_ANSWER_STRINGS[176];
-extern undefined UNKNOWN_MENU_1[72];
+extern struct quiz_answer_points_entry QUIZ_ANSWER_POINTS[174];
+extern undefined QUIZ_DEBUG_MENU[72];
+extern uint16_t QUIZ_QUESTION_ANSWER_ASSOCIATIONS[66];
 
 #endif
