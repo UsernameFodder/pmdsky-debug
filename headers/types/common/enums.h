ffc04d9bb0baa26ef87b04d55ff3977825332f6e
UsernameFodder <usernamefodder@gmail.com>
2023-05-28 13:37:27 -0500

    Port Adex's script_variable_meanings.txt
    Mostly a verbatim port, with minor tweaks and additions.

--- headers/types/common/enums.h
+++ headers/types/common/enums.h
@@ -72,12 +72,13 @@ enum texture_format {
 
 // Values for GAME_MODE
 enum game_mode {
-    GAME_MODE_MENU = 0, // top menu
-    GAME_MODE_1 = 1,
+    GAME_MODE_MENU = 0,            // top menu
+    GAME_MODE_1 = 1,               // also related to special episodes? But 3 is the main one
     GAME_MODE_NORMAL = 2,          // normal play, including both overworld/dungeons
     GAME_MODE_SPECIAL_EPISODE = 3, // during special episodes
     // Pelipper Island (and likely also in the dungeon while rescuing, need to confirm)
     GAME_MODE_RESCUE = 4,
+    GAME_MODE_5 = 5,
     // Possibly more?
 };
 
