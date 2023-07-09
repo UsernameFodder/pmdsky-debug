833ddf6fd1e06ca3f88872ba19d16e08bd3f4045
UsernameFodder <usernamefodder@gmail.com>
2023-07-09 12:23:50 -0500

    Minor tweaks
    - Add top-level docstring
    - Use make -C instead of pushd/popd
    - Use --release flag with cargo run
    - Use '*' instead of '**' for globbing

--- check_and_format.sh
+++ check_and_format.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# Convenience script that runs the checks and formatters for both the C headers and symbol tables.
+# Expects the working directory to be the repo root.
+
 set -e
 
-pushd headers
-make
-make format
-make symbol-check
-popd
+make -C headers
+make -C headers format
+make -C headers symbol-check
 
-cargo run -- check -r -Vvbomu -d screaming_snake_case -f pascalcase -f snake_case symbols/**.yml
-cargo run -- fmt -r symbols/**.yml
+cargo run --release -- check -r -Vvbomu -d screaming_snake_case -f pascalcase -f snake_case symbols/*.yml
+cargo run --release -- fmt -r symbols/*.yml
+
