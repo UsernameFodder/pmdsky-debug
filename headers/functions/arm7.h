588cddd15a31cec9519624b1aabc891392ed5dbe
adakite1 <adakite.a@gmail.com>
2023-11-12 12:06:54 +0800

    Arm7 memory mapping documentation, __divsi3, __udivsi3, and __udivsi3_no_zero_check documentation, FileRead limitations documentation, and some small note updates

--- headers/functions/arm7.h
+++ headers/functions/arm7.h
@@ -2,11 +2,15 @@
 #define HEADERS_FUNCTIONS_ARM7_H_
 
 void EntryArm7(void);
+void MainArm7(void);
 int ClearIrqFlag(void);
 int EnableIrqFlag(void);
 int SetIrqFlag(int new_value);
 int EnableIrqFiqFlags(void);
 int SetIrqFiqFlags(int new_value);
 int GetProcessorMode(void);
+unsigned long long __divsi3(int dividend, int divisor);
+unsigned long long __udivsi3(uint32_t dividend, uint32_t divisor);
+unsigned long long __udivsi3_no_zero_check(uint32_t dividend, uint32_t divisor);
 
 #endif
