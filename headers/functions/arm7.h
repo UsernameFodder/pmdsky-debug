35cf732d8f4d01493567ed20c040faf6101f41b6
Adakite <88466388+adakite1@users.noreply.github.com>
2023-11-13 04:46:55 +0800

    Further Arm7 documentation, as well as a note on FileRead limitations (#206)
    Changed the arm7 addresses to reflect addresses after the memory
    map inside of the entry function as these addresses are the ones
    used by function calls and accessing constants.
    
    I haven't had a chance to take a closer look at the 0x27E0000 area
    since most of the code I've had to work with for arm7 patching has
    been within the 0x37F8000 area, but given that 0xF608 is just under
    64kB which is the amount of WRAM reserved specifically for arm7
    (see https://problemkaputt.de/gbatek.htm#dsmemorymaps), and the
    0x27E0000 area has a much larger size than the WRAM area, I think
    this is a good hypothesis for now.
    
    There were also some small changes I've made:
    
    * [arm7] The DSE audio engine is entirely on the arm9 and does IPC
      with arm7 only as a way to get the audio chip to kick in, so in this way,
      the arm7 just handles audio I/O with the mitsumi MM3205B sound chip,
      setting a few registers when called. Apparently Nintendo didn't allow
      anyone to touch arm7 and add stuff to it? I've changed the note on the
      binary description to reflect this.
    * [arm7] While working on arm7 patches, at one point, I found a need for
      a __divsi3 function, and so after some fuzzing I've found __divsi3,
      __udivsi3, and __udivsi3_no_zero_check on arm7. They worked as
      intended in the patch, and appears to be identical to the arm9 version as
      well bar a few constant accesses. It might be possible to extrapolate some
      more standard library functions from here.
    * [arm9] In early versions of adpcm streaming, I had the decoder on arm7
      ipc arm9 for new ADPCM chunks since only arm9 can read nitrofs files,
      but when arm7 called arm9, the call would go through but after all the
      read parameters had been calculated and once it was time to actually
      read the next block from the rom, it would freeze on FileRead (this is with
      or without calling DataTransferInit and DataTransferStop). I was confused
      for a while, but noticed that the irq handler runs in the irq processor
      mode, not the usual SYSTEM mode where FileReads are generally done.
      Lo-and-behold I could get FileRead to work once again by forcing cpsr
      into SYSTEM mode. This corrupts states of course and crashes the game
      after a few seconds, but the file read itself worked. So I'm not entirely
      sure why, but FileRead appears to only work in some processor mode, so
      I've added a note on that.

--- headers/functions/arm7.h
+++ headers/functions/arm7.h
@@ -2,11 +2,7 @@
 #define HEADERS_FUNCTIONS_ARM7_H_
 
 void EntryArm7(void);
-int ClearIrqFlag(void);
-int EnableIrqFlag(void);
-int SetIrqFlag(int new_value);
-int EnableIrqFiqFlags(void);
-int SetIrqFiqFlags(int new_value);
+void NitroSpMain(void);
 int GetProcessorMode(void);
 
 #endif
