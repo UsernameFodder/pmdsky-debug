ffe2bf8dccd83ca3dc82a50f60073d26543f293d
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2023-11-22 14:30:50 -0600

    Added ARM7 suffix to core SDK functions

--- headers/functions/arm7.h
+++ headers/functions/arm7.h
@@ -2,6 +2,8 @@
 #define HEADERS_FUNCTIONS_ARM7_H_
 
 void _start_arm7(void);
+void do_autoload_arm7(void);
+void StartAutoloadDoneCallbackArm7(void);
 void NitroSpMain(void);
 void HardwareInterrupt(void);
 void ReturnFromInterrupt(void);
