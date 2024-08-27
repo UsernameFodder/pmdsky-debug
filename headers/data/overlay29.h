ba8b0cf1bd5c0b93080e3d7a28289f14d1ee2694
assidion2 <anthonysigov@Anthonys-MacBook-Pro.local>
2024-08-27 11:27:19 +0400

    new findings

--- headers/data/overlay29.h
+++ headers/data/overlay29.h
@@ -102,6 +102,9 @@ extern struct tile DEFAULT_TILE;
 extern bool HIDDEN_STAIRS_SPAWN_BLOCKED;
 extern void* FIXED_ROOM_DATA_PTR;
 extern struct dungeon_fades* DUNGEON_FADES_PTR;
-extern uint32_t POSITIONS_COUNTER;
+extern uint32_t AI_THROWN_ITEM_ACTION_CHOICE_COUNT;
+extern bool DIRECTION_BOOLS[8];
+extern uint32_t THROWN_ITEM_DIRECTIONS[8];
+extern uint32_t THROWN_ITEM_PROBABILITIES[8];
 
 #endif
