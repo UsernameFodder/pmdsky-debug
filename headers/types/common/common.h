d776c78e194fb2a0fcff32a72f05922e0acad0f0
assidion <sigov@berkeley.edu>
2025-02-06 19:14:04 +0400

    fix

--- headers/types/common/common.h
+++ headers/types/common/common.h
@@ -857,7 +857,7 @@ struct mission_rescue_bin {
     undefined field_0x1f;
     struct mission_type_8 type;                 // 0x20
     union mission_subtype subtype;              // 0x21
-}
+};
 
 // Unverified, ported from Irdkwia's notes
 struct quiz_answer_points_entry {
