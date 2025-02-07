f48dbd7ca6b3e9190d50c44c72a3d21d42ef2a1b
assidion2 <sigov@berkeley.edu>
2025-02-08 03:42:36 +0400

    Challenge letter research (#307)

--- headers/types/common/common.h
+++ headers/types/common/common.h
@@ -795,11 +795,13 @@ struct mission {
     int description_id;
     uint8_t unique_map_id; // 0xC: for challenges/treasure hunts/certain outlaws
     undefined field_0xd;
-    struct monster_id_16 client;                // 0xE
-    struct monster_id_16 target;                // 0x10
-    struct monster_id_16 outlaw_backup_species; // 0x12
-    struct item_id_16 item_wanted;              // 0x14
-    struct mission_reward_type_8 reward_type;   // 0x16
+    struct monster_id_16 client; // 0xE: For challenge letter missions, stores the leader
+    // 0x10: For non-legendary challenge letter missions, stores the second team member
+    struct monster_id_16 target;
+    // 0x12: For non-legendary challenge letter missions, stores the third team member
+    struct monster_id_16 outlaw_backup_species;
+    struct item_id_16 item_wanted;            // 0x14
+    struct mission_reward_type_8 reward_type; // 0x16
     undefined field_0x17;
     struct item_id_16 item_reward;                      // 0x18
     struct mission_restriction_type_8 restriction_type; // 0x1A
@@ -815,6 +817,47 @@ struct mission_floors_forbidden {
     uint8_t field_0x1;
 };
 ASSERT_SIZE(struct mission_floors_forbidden, 2);
+
+// Information about a valid mission; a list of these structs is stored in and directly loaded from
+// RESCUE/rescue.bin. Used for validation as well as possibly for loading certain missions?
+struct mission_rescue_bin {
+    undefined field_0x0;
+    undefined field_0x1;
+    undefined field_0x2;
+    undefined field_0x3;
+    undefined field_0x4;
+    undefined field_0x5;
+    undefined field_0x6;
+    undefined field_0x7;
+    // 0x8: For legendary challenge missions, is used as a boolean to indicate whether or not
+    // accepting the mission unlocks its dungeon. For other missions, its purpose is unknown.
+    uint8_t unk_unlock_flag;
+    undefined field_0x9;
+    undefined field_0xa;
+    undefined field_0xb;
+    int8_t restricted_dungeon; // 0xC: Dungeon this mission is restricted to, or 0xFF if none
+    undefined field_0xd;
+    undefined field_0xe;
+    undefined field_0xf;
+    undefined field_0x10;
+    undefined field_0x11;
+    struct monster_id_16 client; // 0x12: For challenge letter missions, stores the leader
+    undefined field_0x14;
+    undefined field_0x15;
+    undefined field_0x16;
+    undefined field_0x17;
+    // 0x18: For non-legendary challenge letter missions, stores the second team member
+    struct monster_id_16 target;
+    undefined field_0x1a;
+    undefined field_0x1b;
+    undefined field_0x1c;
+    undefined field_0x1d;
+    // 0x1E: For non-legendary challenge letter missions, stores the third team member
+    struct monster_id_16 outlaw_backup_species;
+    struct mission_type_8 type;    // 0x20
+    union mission_subtype subtype; // 0x21
+};
+ASSERT_SIZE(struct mission_rescue_bin, 34);
 
 // Unverified, ported from Irdkwia's notes
 struct quiz_answer_points_entry {
