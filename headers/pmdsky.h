8aa54230a70f0b3b1b9335c4284c7350edc98dce
UsernameFodder <93669634+UsernameFodder@users.noreply.github.com>
2025-02-12 17:51:05 -0600

    Use signed char for int8_t (#309)
    The char type is special in that the C spec allows it to be either
    signed or unsigned depending on the target platform. Explicitly use
    signed char to defined int8_t instead, for portability.
    
    Bump the release version to 0.10.1 with this fix.

--- headers/pmdsky.h
+++ headers/pmdsky.h
@@ -43,7 +43,7 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
-typedef char int8_t;
+typedef signed char int8_t;
 typedef short int16_t;
 typedef int int32_t;
 typedef long long int64_t;
