03ac86aaa7d8ccd8e40c1c68492c9843be171834
HappyLappy1 <86489014+HappyLappy1@users.noreply.github.com>
2025-08-24 13:43:51 -0400

    Update overlay21.h

--- headers/functions/overlay21.h
+++ headers/functions/overlay21.h
@@ -5,7 +5,7 @@ void SwapShopDialogueManager(int current_switch_case);
 enum item_id GetFirstExclusivePrerequisite(enum item_id item_id);
 int SwapShopEntryPoint(void);
 void SwapShopDestructor(void);
-void SwapShopMainManager(void);
+int SwapShopMainManager(void);
 void CloseTextboxAndSimpleMenu(void);
 void SwapShopPrintCurrentGold(int window_id);
 
