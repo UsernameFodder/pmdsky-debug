e4f5daf8768869941d5a560df1f1478136b27755
HappyLappy1 <86489014+HappyLappy1@users.noreply.github.com>
2025-08-24 19:45:41 -0400

    Update overlay21.h

--- headers/functions/overlay21.h
+++ headers/functions/overlay21.h
@@ -1,7 +1,7 @@
 #ifndef HEADERS_FUNCTIONS_OVERLAY21_H_
 #define HEADERS_FUNCTIONS_OVERLAY21_H_
 
-void SwapShopDialogueManager(int current_switch_case);
+void SwapShopDialogueManager(enum swap_shared_case new_shared_case);
 enum item_id GetFirstExclusivePrerequisite(enum item_id item_id);
 int SwapShopEntryPoint(void);
 void SwapShopDestructor(void);
