10cd93ceb075a0ee90d7e52aec15cc491e068416
assidion <sigov@berkeley.edu>
2025-08-31 23:53:53 +0400

    a

--- headers/types/common/window.h
+++ headers/types/common/window.h
@@ -326,7 +326,7 @@ struct window_list {
 ASSERT_SIZE(struct window_list, 4480);
 
 // Tracks state related to interactive windows.
-// A pointer to this struct is always at +0x4 of the various window contents structs stored in window_params / returned by GetWindowContents.
+// A pointer to this struct seems to always be at +0x4 of the various window contents structs stored in window_params / returned by GetWindowContents.
 struct window_input_ctx {
     undefined fields_0x0[180];
     uint32_t first_item_y_offset; // 0xB4: Y-offset for first item on page
