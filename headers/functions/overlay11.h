3e723e16d5b7d98e7e9d2b456cac5499409b711a
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2025-08-31 12:20:47 -0400

    Documented some ground BG functions, filled in language addresses (#338)

--- headers/functions/overlay11.h
+++ headers/functions/overlay11.h
@@ -38,8 +38,14 @@ bool ReturnToTitleScreen(uint32_t fade_duration);
 void ScriptSpecialProcess0x16(bool param_1);
 bool IsScreenFadeInProgress(void);
 void LoadBackgroundAttributes(struct bg_list_entry* entry, int bg_id);
+void GroundBgInit(struct ground_bg* ground_bg, const struct ground_bg_sub_struct_2bc* param_2);
+void GroundBgFreeAll(struct ground_bg* ground_bg);
+void GroundBgCloseOpenedFiles(struct ground_bg* ground_bg);
 void LoadMapType10(void* buf, int map_id, undefined* dungeon_info_str, undefined4 additional_info);
 void LoadMapType11(void* buf, int map_id, undefined* dungeon_info_str, undefined4 additional_info);
+const uint8_t* BmaLayerNrlDecompressor(uint16_t** dst_array, const uint8_t* bma_data,
+                                       struct ground_bg_sub_struct_2bc* param_3,
+                                       struct bma_header* bma_header);
 void GetSpecialLayoutBackground(int bg_id, undefined* dungeon_info_str, undefined4 additional_info,
                                 bool copy_fixed_room_layout);
 void SetAnimDataFields(struct animation* anim, uint16_t param_2);
