9825d350e20459243407b48257517a47925fe7d0
assidion <sigov@berkeley.edu>
2026-06-07 16:16:59 +0400

    aaa

--- headers/functions/overlay29.h
+++ headers/functions/overlay29.h
@@ -214,7 +214,8 @@ bool IsSecretBazaarNpcBehavior(enum monster_behavior behavior);
 bool TalkToSecretBazaarNpcStandard(int string_id, struct entity* shopkeeper,
                                    enum portrait_emotion emotion);
 bool TalkToSecretBazaarNpcWithYesNoMenu(int string_id, struct entity* shopkeeper,
-                                        enum portrait_emotion emotion);
+                                        enum portrait_emotion emotion,
+                                        struct simple_menu_id_item* menu_items);
 void MakeTargetFaceUserAndIdle(struct entity* target, struct entity* user);
 void SubtractMoneyCarriedWithSfx(int amount);
 bool TalkToSecretBazaarNpc(int string_id, struct entity* shopkeeper, enum portrait_emotion emotion,
