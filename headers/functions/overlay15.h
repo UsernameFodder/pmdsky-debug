a4af5d78dea5073d3aea12d856757fb358e1328e
happylappy <86489014+HappyLappy1@users.noreply.github.com>
2026-06-17 23:03:12 -0400

    Duskull Bank Functions and Data

---
+++ headers/functions/overlay15.h
@@ -0,0 +1,13 @@
+#ifndef HEADERS_FUNCTIONS_OVERLAY15_H_
+#define HEADERS_FUNCTIONS_OVERLAY15_H_
+void InitDuskullBankStruct(void);
+undefined4 DuskullBankSubcaseManager1(void);
+void DuskullBankSubcaseManager2(enum bank_subcase subcase);
+void BankUpdateDigitInputMenuDisplay(bool is_withdrawing);
+void DuskullBankTextboxGoldStatusCallback(int window_id);
+void DuskullBankDigitInputDisplayCallback(void);
+undefined4 DuskullBankEntryPoint(void);
+void DuskullBankDestructor(void);
+undefined4 DuskullBankFrameUpdate(void);
+
+#endif
