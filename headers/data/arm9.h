e20d712974c978a1041e200cb619286ff6f834a4
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2026-07-02 20:50:23 -0400

    Filled language addresses

--- headers/data/arm9.h
+++ headers/data/arm9.h
@@ -282,6 +282,7 @@ extern int32_t MUSIC_DURATION_LOOKUP_TABLE_2[128];
 extern sound_lfo_waveform_callback LFO_WAVEFORM_CALLBACKS[16];
 extern bool IS_DISP_ON;
 extern uint32_t GXI_DMA_ID;
+extern uint32_t OSI_CONSOLE_TYPE_CACHE;
 extern uint32_t RAND_SEQUENCE_NUM;
 
 #endif
